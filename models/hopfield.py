f66d95e49fada4e7ce892d7a0e3a2e48789ed71a
Isidro Henoch <isidro.henoch@gmail.com>
2018-10-24 23:43:56 -0500

    Fixes the hopfield's storage function

--- models/hopfield.py
+++ models/hopfield.py
@@ -1,14 +1,23 @@
 w = []
+set_a = []
+set_b = []
+I = []
 
-def storage(set_a, set_b):
+def storage(_set_a, _set_b):
     global w
+    global set_a
+    global set_b
+    global I
+    set_a = _set_a
+    set_b = _set_b
     a, b = len(set_a[0]), len(set_b[0])
     w = [[0 for x in range(b)] for y in range(a)]
+    I = [[0 for x in range(b)] for y in range(a)]
     for i in range(a):
         for j in range(b):
             sum = 0
-            for xy_index in range(len(set_a)):
-                x = set_a[xy_index]
-                y = set_b[xy_index]
-                sum += x[i] * y[j]
+            if i == j:
+                I[i][j] = 1
+            else:
+                sum = set_a[0][i] * set_a[0][i] + set_b[0][j] * set_b[0][j] - 2 * I[i][j]
             w[i][j] = sum
