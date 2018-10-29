4076235b2e3a3a462d81fda4dd66dd34e85e36f2
Isidro Henoch <isidro.henoch@gmail.com>
2018-10-28 23:39:52 -0600

    Fixes the initialize function and adds the euclidean distance function

--- models/kohonen.py
+++ models/kohonen.py
@@ -1,5 +1,10 @@
 import random
+from functools import reduce
 
 # Generates j initial weights with n values between 0-1
 def initialize(j, n):
-    [[random.uniform(0.0, 1.0) for _ in range(j)] for _ in range(n)]
+    return [[random.uniform(0.0, 1.0) for _ in range(j)] for _ in range(n)]
+
+# Calculates the euclidean distance between two vectors
+def euclideanDistance(x, w):
+    return reduce((lambda i, acc: i + acc), [(xi - wi)**2 for xi, wi in zip(x, w)])**(1/2)
